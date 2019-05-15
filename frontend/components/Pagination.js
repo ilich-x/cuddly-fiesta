0f96415643dfbb5daaf14ef7929ce44bfdb0d75b
Ilya Nazarov <ilichxx@gmail.com>
2019-05-15 23:31:11 +0300

    start working on pagination

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import Head from 'next/head';
+import Link from 'next/link';
+import { perPage } from '../config';
+import PaginationStyles from './styles/PaginationStyles';
+
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    itemsConnection {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
+
+const Pagination = ({ page }) => (
+  <Query query={PAGINATION_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <p>Loading...</p>;
+      const { count } = data.itemsConnection.aggregate;
+      const pages = Math.ceil(count / perPage);
+
+      return (
+        <PaginationStyles>
+          <Head>
+            <title>
+              Sick fits! Page {page} of {pages}
+            </title>
+          </Head>
+          <Link
+            prefetch
+            href={{
+              pathname: 'items',
+              query: { page: page - 1 },
+            }}
+          >
+            <a className="prev" aria-disabled={page <= 1}>
+              &#8592; Prev
+            </a>
+          </Link>
+          <p>
+            Page {page} of {pages}
+          </p>
+          <p>{count} items total</p>
+          <Link
+            prefetch
+            href={{
+              pathname: 'items',
+              query: { page: page + 1 },
+            }}
+          >
+            <a className="prev" aria-disabled={page >= pages}>
+              Next &#8594;
+            </a>
+          </Link>
+        </PaginationStyles>
+      );
+    }}
+  </Query>
+);
+
+export default Pagination;
