b25b935a19ae95893937f97eb1c71230c6b4f6b6
Ilya Nazarov <ilichxx@gmail.com>
2019-07-11 17:16:28 +0300

    tests request reset

--- frontend/components/RequestReset.js
+++ frontend/components/RequestReset.js
@@ -27,6 +27,7 @@ class RequestReset extends Component {
       <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
         {(reset, { error, loading, called }) => (
           <Form
+            data-test="form"
             method="post"
             onSubmit={async e => {
               e.preventDefault();
@@ -60,3 +61,4 @@ class RequestReset extends Component {
 }
 
 export default RequestReset;
+export { REQUEST_RESET_MUTATION };
